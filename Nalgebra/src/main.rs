2028d72dc0a60c2b55cbdbbab297ef104b6135a0
nam2ee <nyj5404@yonsei.ac.kr>
2024-01-13 20:17:02 +0900

    first

---
+++ Nalgebra/src/main.rs
@@ -0,0 +1,14 @@
+use nalgebra::{U2, U3, Dynamic, ArrayStorage, VecStorage, Matrix, Vector3, Matrix3x4};
+
+
+
+fn main() {
+    println!("Hello, world!");
+
+    let v = Vector3::new(1,2,3);
+
+    let m = Matrix3x4::new(11,12,13,14,
+    21,22,23,24,
+31,32,33,34);
+
+}
